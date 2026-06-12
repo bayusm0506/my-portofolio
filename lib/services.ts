a47553024fbe197d66943dca4bf943c1885a4d60
Bayu Setra Maulana <bayu.setramaulana@gmail.com>
2026-06-12 21:45:42 +0700

    fix: fix: split service & interface of blogs module

--- lib/services.ts
+++ lib/services.ts
@@ -1,7 +1,16 @@
-import { ProjectsData } from '@/interfaces/global-interfaces';
+import { BlogPost, ProjectsData } from '@/interfaces/global-interfaces';
 
 export async function fetchProjects(): Promise<ProjectsData[]> {
   const res = await fetch('/api/projects');
+  const response = await res.json();
+
+  if (!response.success) throw new Error(response.message);
+
+  return response.data;
+}
+
+export async function fetchBlogs(): Promise<BlogPost[]> {
+  const res = await fetch('/api/blog');
   const response = await res.json();
 
   if (!response.success) throw new Error(response.message);
